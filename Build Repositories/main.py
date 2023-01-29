f9dcc8702243207b8fd8ffef9c4f4505e0e2a990
Abdulmoeez Kamran <moiz.kamran28@gmail.com>
2023-01-30 01:53:17 +0500

    Initial commit

---
+++ Build Repositories/main.py
@@ -0,0 +1,30 @@
+from PIL import Image
+
+# Dictionary that maps each t-shirt image to its corresponding overlay image
+overlay_images = {
+    "mockups/tshirt1.jpg": "graphic/graphic.png",
+    "mockups/tshirt2.jpg": "graphic/graphic.png",
+    "mockups/tshirt3.jpg": "graphic/graphic_black.png",
+    "mockups/tshirt4.jpg": "graphic/graphic.png"
+}
+
+
+# Dictionary that maps each t-shirt image to its corresponding position and scale
+tshirt_positions_scale = {
+    "mockups/tshirt1.jpg": ((781, 372), (490,490)),
+    "mockups/tshirt2.jpg": ((2535, 1004), (1500,1500)),
+    "mockups/tshirt3.jpg": ((1680, 1116), (580,580)),
+    "mockups/tshirt4.jpg": ((618, 448), (791,791))
+}
+
+# Iterate over the t-shirt images
+for tshirt_img, (position,scale) in tshirt_positions_scale.items():
+    overlay_img = Image.open(overlay_images[tshirt_img])
+    overlay_img = overlay_img.convert("RGBA")
+    # Open the t-shirt image
+    tshirt = Image.open(tshirt_img)
+    overlay_img = overlay_img.resize(scale)
+    # Position the overlay image at the desired location on the t-shirt image
+    tshirt.paste(overlay_img, position, overlay_img)
+    # Save the resulting image
+    tshirt.save("modified_" + tshirt_img)
---
+++ README.md
@@ -0,0 +1,2 @@
+# Python-Mockup-Generator
+ A python script that takes t-shirt images and applies an overlay graphic to each of them at specified positions and scales.
